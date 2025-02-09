5cb0fe127c866fd9d2b1a90c1c6a2f48d9ff6733
chris-de-leon <chrisdeleon333+dev@gmail.com>
2025-02-08 21:30:09 -0800

    feat: add secrets CLI

--- flake.nix
+++ flake.nix
@@ -9,20 +9,33 @@
 {
   inputs = {
     nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/e032e7ed264d9cae8793b947fce8c6205efeb272.tar.gz";
+    secrets.url = "https://github.com/chris-de-leon/secrets-cli/archive/refs/tags/v1.1.0.tar.gz";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem(system:
+  outputs = { self, nixpkgs, secrets, utils }:
+    utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-      in {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              scli = secrets.defaultPackage.${prev.system};
+            })
+          ];
+        };
+      in
+      rec {
+        formatter = pkgs.nixpkgs-fmt;
+
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.nodejs.pkgs.pnpm
+            pkgs.lastpass-cli
             pkgs.nodejs
             pkgs.sqlite
             pkgs.redis
+            pkgs.scli
           ];
         };
       }
